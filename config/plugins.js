c5631ff63f4ca61be33e207b5ea4af7d4f254875
Theo Ferguson <ferguson.theo@gmail.com>
2023-02-16 13:23:38 -0500

    debug cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,18 @@
 module.exports = ({ env }) => ({
     // ...
     upload: {
-      provider: 'cloudinary-folderoptions',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-        default_folder: eng('CLOUDINARY_DEFAULT_FOLDER'),
-      },
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: env('CLOUDINARY_NAME'),
+                api_key: env('CLOUDINARY_KEY'),
+                api_secret: env('CLOUDINARY_SECRET'),
+            },
+            actionOptions: {
+                upload: {},
+                delete: {},
+            },
+        },
     },
     // ...
-  });
+});
